945881465959c57c53ae177cdcbd8eb2b2b85d8e
Cristian Santiz <crisantizan@gmail.com>
2020-08-28 21:08:10 -0500

    chore: SET_CHECKED mutation

--- store/users.js
+++ store/users.js
@@ -1,10 +1,28 @@
 export const state = () => ({
   users: [],
+  amount: { California: 3, Immigration: 2 },
 });
 
 export const mutations = {
   SET_USERS(state, payload) {
     state.users = payload;
+  },
+
+  SET_CHECKED(state, { id, checked, library }) {
+    // search user
+    const user = state.users.find(user => user.id === id);
+
+    if (!user) {
+      return;
+    }
+
+    user.assignLibraries.forEach((val, index) => {
+      // set new value
+      if (val === library) {
+        user.assignLibraries[index] = checked;
+        return;
+      }
+    });
   },
 };
 
