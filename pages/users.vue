b246d678bb4bd67de94ae64b29be4af1a904d9db
Cristian Santiz <crisantizan@gmail.com>
2020-08-29 08:10:54 -0500

    feat: table-wrapper

--- pages/users.vue
+++ pages/users.vue
@@ -4,117 +4,123 @@
     <div class="card-box mt-2">
       <!-- <datatable :headers="headers" :columns="users" /> -->
       <client-only>
-        <!-- custom header -->
-        <div class="d-flex mb-2">
-          <div class="search btn-group mr-2">
-            <input
-              class="form-control search-input"
-              type="text"
-              placeholder="Search"
-              autocomplete="off"
-              v-model="searchTerm"
-            />
-            <div class="btn btn-primary" style="cursor: default;">
-              <i class="fa fa-search" aria-hidden="true"></i>
+        <div class="table-wrapper">
+          <!-- custom header -->
+          <div class="d-flex mb-2">
+            <div class="search btn-group mr-2">
+              <input
+                class="form-control search-input"
+                type="text"
+                placeholder="Search"
+                autocomplete="off"
+                v-model="searchTerm"
+              />
+              <div class="btn btn-primary" style="cursor: default;">
+                <i class="fa fa-search" aria-hidden="true"></i>
+              </div>
             </div>
-          </div>
 
-          <div class="dropdown">
-            <button
-              class="btn btn-primary dropdown-toggle"
-              type="button"
-              id="dropdownMenuButton"
-              data-toggle="dropdown"
-              aria-haspopup="true"
-              aria-expanded="false"
-            >
-              <i class="fa fa-th-list" aria-hidden="true"></i>
-            </button>
+            <div class="dropdown">
+              <button
+                class="btn btn-primary dropdown-toggle"
+                type="button"
+                id="dropdownMenuButton"
+                data-toggle="dropdown"
+                aria-haspopup="true"
+                aria-expanded="false"
+              >
+                <i class="fa fa-th-list" aria-hidden="true"></i>
+              </button>
 
-            <div class="dropdown-menu p-0" aria-labelledby="dropdownMenuButton">
-              <form @submit.prevent>
-                <div
-                  v-for="column of columns"
-                  :key="column.field"
-                  class="dropdown-item px-0"
-                >
+              <div
+                class="dropdown-menu p-0"
+                aria-labelledby="dropdownMenuButton"
+              >
+                <form @submit.prevent>
                   <div
-                    v-if="column.toggle"
-                    class="custom-control custom-checkbox mx-1"
+                    v-for="column of columns"
+                    :key="column.field"
+                    class="dropdown-item px-0"
                   >
-                    <input
-                      type="checkbox"
-                      class="custom-control-input"
-                      :id="`toggle${column.field}`"
-                      :checked="!column.hidden"
-                      @change="e => (column.hidden = !column.hidden)"
-                    />
-                    <!-- v-model="column.hidden" -->
-                    <label
-                      class="custom-control-label d-flex align-items-center"
-                      :for="`toggle${column.field}`"
+                    <div
+                      v-if="column.toggle"
+                      class="custom-control custom-checkbox mx-1"
                     >
-                      {{ column.label }}
-                    </label>
+                      <input
+                        type="checkbox"
+                        class="custom-control-input"
+                        :id="`toggle${column.field}`"
+                        :checked="!column.hidden"
+                        @change="e => (column.hidden = !column.hidden)"
+                      />
+                      <!-- v-model="column.hidden" -->
+                      <label
+                        class="custom-control-label d-flex align-items-center"
+                        :for="`toggle${column.field}`"
+                      >
+                        {{ column.label }}
+                      </label>
+                    </div>
                   </div>
-                </div>
-              </form>
+                </form>
+              </div>
             </div>
           </div>
-        </div>
 
-        <!-- end custom header -->
+          <!-- end custom header -->
 
           <!-- styleClass="table my-table table-hover" -->
-        <vue-good-table
-          styleClass="vgt-table my-table table-hover"
-          :columns="columns"
-          :rows="users"
-          compactMode
-          :pagination-options="{
-            enabled: true,
-            mode: 'pages',
-            perPage: 5,
-            perPageDropdown: [5, 10, 15, 20],
-          }"
-          :search-options="{
-            enabled: true,
-            externalQuery: searchTerm,
-          }"
-        >
-          <template slot="table-row" slot-scope="props">
-            <template
-              v-if="
-                props.column.field == 'assignLibraries' && !props.column.hidden
-              "
-            >
-              <div
-                v-for="library in availableLibraries"
-                :key="library"
-                class="custom-control custom-checkbox d-flex"
+          <vue-good-table
+            styleClass="vgt-table my-table table-hover"
+            :columns="columns"
+            :rows="users"
+            compactMode
+            :pagination-options="{
+              enabled: true,
+              mode: 'pages',
+              perPage: 5,
+              perPageDropdown: [5, 10, 15, 20],
+            }"
+            :search-options="{
+              enabled: true,
+              externalQuery: searchTerm,
+            }"
+          >
+            <template slot="table-row" slot-scope="props">
+              <template
+                v-if="
+                  props.column.field == 'assignLibraries' &&
+                    !props.column.hidden
+                "
               >
-                <input
-                  type="checkbox"
-                  class="custom-control-input"
-                  :id="generateCheckboxId(library, props.formattedRow.id)"
-                  :value="library"
-                  :checked="
-                    isChecked(props.formattedRow.assignLibraries, library)
-                  "
-                />
-                <label
-                  class="custom-control-label d-flex align-items-center"
-                  :for="generateCheckboxId(library, props.formattedRow.id)"
+                <div
+                  v-for="library in availableLibraries"
+                  :key="library"
+                  class="custom-control custom-checkbox d-flex"
                 >
-                  {{ library }}
-                </label>
-              </div>
+                  <input
+                    type="checkbox"
+                    class="custom-control-input"
+                    :id="generateCheckboxId(library, props.formattedRow.id)"
+                    :value="library"
+                    :checked="
+                      isChecked(props.formattedRow.assignLibraries, library)
+                    "
+                  />
+                  <label
+                    class="custom-control-label d-flex align-items-center"
+                    :for="generateCheckboxId(library, props.formattedRow.id)"
+                  >
+                    {{ library }}
+                  </label>
+                </div>
+              </template>
+              <span v-else>
+                {{ props.formattedRow[props.column.field] }}
+              </span>
             </template>
-            <span v-else>
-              {{ props.formattedRow[props.column.field] }}
-            </span>
-          </template>
-        </vue-good-table>
+          </vue-good-table>
+        </div>
       </client-only>
     </div>
   </div>
