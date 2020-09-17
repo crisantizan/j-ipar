c26ad0a42e744c0cf7b380957ef0c437a3b27af7
Cristian Santiz <crisantizan@gmail.com>
2020-09-17 09:08:27 -0500

    styles: print tenant data in card

--- pages/index.vue
+++ pages/index.vue
@@ -1,8 +1,25 @@
 <template>
-	<div>
-		<h1>Home</h1>
-		<img :src="imageUrl" alt="">
-		<pre>{{ tenant }}</pre>
+	<div class="d-flex justify-content-center">
+		<!-- card -->
+		<div class="card bg-light mt-3" style="width: 18rem">
+			<!-- card image -->
+			<img class="card-img-top" :src="imageUrl" alt="Card image cap" />
+			<!-- card body -->
+			<div class="card-body">
+				<h5 class="card-title">{{ tenant.name }}</h5>
+				<!-- billing data -->
+				<div class="d-flex justify-content-between">
+					<h6 class="card-subtitle mb-2 text-muted">
+						<i class="fas fa fa-user mr-1"></i>{{ fullname }}
+					</h6>
+					<h6 class="card-subtitle mb-2 text-muted">
+						<i class="fas fa fa-map-marker mr-1"></i>{{ address }}
+					</h6>
+				</div>
+				<!-- end billing data -->
+			</div>
+			<!-- end card body -->
+		</div>
 	</div>
 </template>
 
@@ -12,9 +29,24 @@ import { mapGetters } from 'vuex';
 export default {
 	computed: {
 		...mapGetters('tenant', ['tenant']),
+
+		fullname() {
+			return `${this.tenant.billingFirstName} ${this.tenant.billingLastName}`;
+		},
+
+		address() {
+			return this.tenant.billingAddress || 'Billing Address'
+		},
+
 		imageUrl() {
 			return `${process.env.BASE_IMAGE_URL}/${this.tenant.logoUrl}`;
-		}
+		},
 	},
 };
 </script>
+
+<style scoped>
+	.card {
+		border: 1px solid rgba(0, 0, 0, 0.1);
+	}
+</style>
